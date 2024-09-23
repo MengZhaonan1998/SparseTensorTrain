00d07e8a41f352386fd14515882a8762b61dc429
MengMeng <mengzn1998@gmail.com>
2024-09-23 15:36:08 -0400

    test TT-Cross on Synthetic tensors

--- Python_Code/TTCross.py
+++ Python_Code/TTCross.py
@@ -5,18 +5,57 @@ import random as rd
 from tensorly.contrib.decomposition import tensor_train_cross
 from tensorly.decomposition import tensor_train
 
-N = 5 * 5 * 5
-sparsity = 0.8
-entry = np.zeros(N)
-for i in range(N):
-    r = rd.random()
-    if r > sparsity:
-        entry[i] = rd.random()
+def TT_Cross():
+    
+    return
 
-tensor = tl.tensor(entry.reshape(5,5,5))
-rank = [1, 3, 3, 1]
-factors = tensor_train(tensor, rank)
-rec_tensor = tl.tt_to_tensor(factors)
-error = tl.norm(rec_tensor - tensor)/tl.norm(tensor)
+def SparseInfo(factors: list[np.array]):
+    avgsparsity = 0
+    avgdensity = 0
+    nlist = len(factors)
+    for i in range(nlist):
+        factor = factors[i]
+        cntzero = np.count_nonzero(np.abs(factor) < 1e-15)
+        size = factor.size
+        sparsity = cntzero / size
+        avgsparsity += sparsity
+        density = 1 - sparsity
+        avgdensity += density
+        print(f"Tensor factor {i}: count of zero = {cntzero}, size = {size}, sparsity = {sparsity}, density = {density}")
+    
+    avgsparsity /= nlist
+    avgdensity /= nlist
+    print(f"Factors' average sparsity: {avgsparsity}; average density: {avgdensity}.")
+    
+    if nlist > 1:
+        recon_tensor = tl.tt_to_tensor(factors)
+        cntzero = np.count_nonzero(recon_tensor == 0)
+        size = recon_tensor.size
+        sparsity = cntzero / size
+        density = 1 - sparsity
+        print(f"Reconstruction tensor: count of zero = {cntzero}, size = {size}, sparsity = {sparsity}, density = {density}")
+    return
+
+rank = [1, 5, 5, 1]
+order = [10, 10, 10]
+sparsity = [0.8, 0.8, 0.8]
+factors = []
+for i in range(len(order)):
+    N = rank[i] * order[i] * rank[i+1]
+    factor = np.zeros(N)
+    for j in range(N):
+        r = rd.random()
+        if r > sparsity[i]:
+            factor[j] = rd.random()
+    factor = tl.tensor(factor.reshape(rank[i], order[i], rank[i+1]))
+    factors.append(factor)
+SpT1 = tl.tt_to_tensor(factors)
+SparseInfo([SpT1])
+    
+factors = tensor_train_cross(SpT1, rank)
+rec_SpT1 = tl.tt_to_tensor(factors)
+error = tl.norm(rec_SpT1 - SpT1)/tl.norm(SpT1)
+SparseInfo(factors)
+
 print(error)
 
