83a26d8c4ef49f4c44abfd315d08f535d0b36022
MengMeng <mengzn1998@gmail.com>
2024-09-29 13:40:24 -0400

    interpolative

--- Python_Code/interpolative.py
+++ Python_Code/interpolative.py
@@ -1,12 +1,15 @@
 import numpy as np
 import scipy as sp
-from scipy.linalg.interpolative import interp_decomp
 from scipy.sparse import random
 
-S = random(5, 5, 0.25, )
+from scipy.linalg.interpolative import interp_decomp
+from scipy.linalg.interpolative import reconstruct_interp_matrix
+
+S = random(10, 10, 0.9)
 dS = S.toarray()
 
 k, idx, proj = interp_decomp(dS, 1e-4)
 
+A = reconstruct_interp_matrix(idx, proj)
 
 pass
