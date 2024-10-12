ca66d7f14eae990a0f100e63b46a029eb89512b8
MengMeng <mengzn1998@gmail.com>
2024-10-12 15:52:14 -0400

    All synthetic unit tests

---
+++ Julia_Code/SyntheticTest.jl
@@ -0,0 +1,75 @@
+include("TTID.jl")
+using SparseArrays
+
+function RandomSpT3Gen(order, density)
+  # random sparse tensor in dense format
+  nbar = 1   
+  dim = length(order)      
+  for i in 1:dim
+    nbar *= order[i]
+  end
+  V = sprandn(nbar, density)
+  V = Array(V)
+  V = reshape(V, order)
+  return V
+end
+
+function RandomTTGen(order, rank, density)
+  # random sparse tensor train 
+  factors = []
+  dim = length(order)
+  for i in 1:dim
+    factorOrder = (rank[i], order[i], rank[i+1])
+    factor = RandomSpT3Gen(factorOrder, density[i])  
+    push!(factors, factor)
+  end
+  return factors    
+end
+
+function testCase1()
+  println("Unit Test 1 starts!")
+  order = [6, 3, 7, 5]    
+  rank = [1, 4, 5, 3, 1]
+  density = [0.3, 0.2, 0.3, 0.4] 
+  ttFactors = RandomTTGen(order, rank, density)
+  I = Index(order[1], "index_i")
+  J = Index(order[2], "index_j")
+  K = Index(order[3], "index_k")
+  L = Index(order[4], "index_m")
+  Indices = (I, J, K, L)
+  println("The sparsity info of input TT factors:")
+  for i in 1:length(Indices)
+    TensorSparsityStat(ttFactors[i])
+  end
+  println("The sparsity info of the synthetic input tensor:")
+  Tensor = TTContraction(ttFactors, Indices)
+  
+  TensorSparsityStat(Tensor)
+
+  r_max = 5
+  eps = 1E-4
+  idFactors = TTID(Tensor, r_max, eps)
+  recTensor = TTContraction(idFactors, Indices)
+  err = ErrorEval(Tensor, recTensor)
+  println("The reconstruction error of TTID:")
+  @show err
+
+  # Sparsity statistics
+  println("The sparsity info of output TT factors:")
+  for i in 1:length(Indices)
+    TensorSparsityStat(idFactors[i])
+  end
+  println("Unit Test 1 ends")
+  return
+end
+
+function testCase2()
+
+end
+
+
+let 
+  println("Unit test starts!")
+  testCase1()
+  println("Unit test ends!")
+end
