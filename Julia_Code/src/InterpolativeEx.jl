0a4de9bf021649a462ed89ab16e6c00a4c7dbf89
MengMeng <mengzn1998@gmail.com>
2024-10-23 23:08:28 -0400

    Unit test(example) of interpolative decomposition

--- Julia_Code/src/InterpolativeEx.jl
+++ Julia_Code/src/InterpolativeEx.jl
@@ -2,6 +2,49 @@ using LinearAlgebra
 using Printf
 using ITensorTCI: interpolative
 
+function interpolative_nuclear(M::Matrix; cutoff=0.0, maxdim=min(size(M)))
+  maxdim = min(maxdim, size(M)...)
+  cols = Int[]
+  K = M' * M
+  m = size(K, 1)
+  Kt = K
+  error = 0.0
+  for t in 1:maxdim
+    Kt2 = Kt * Kt
+    l = argmax(p -> (p ∈ cols ? -Inf : Kt2[p, p] / Kt[p, p]), 1:m)
+    max_err2 = Kt2[l, l] / Kt[l, l]
+    push!(cols, l)
+    error = sqrt(abs(max_err2))
+    (max_err2 < cutoff^2) && break
+    Kt = K - K[:, cols] * (K[cols, cols] \ K[cols, :]) #Schur complement
+  end
+  C = M[:, cols]
+  X = C \ M
+
+  for w in 1:length(cols), r in 1:length(cols)
+    X[r, cols[w]] = (r == w) ? 1.0 : 0.0
+  end
+  return C, X, cols, error
+end
+
+let
+  A = [3 1; 8 2; 9 -5; -7 4]
+  B = [4 6 2; 8 -1 -4]
+  M = A * B
+  cutoff = 1E-3
+  maxdim = 2
+  println("M = "); display(M); println()
+
+  C, X, cols, error = interpolative_nuclear(M; cutoff, maxdim)
+
+  println("C = "); display(C); println()
+  println("X = "); display(X); println()
+  @printf("Two-norm error = %.3E\n", norm(C*X - M, 2))
+ @printf("∞-norm error = %.3E\n", norm(C*X - M, Inf))
+
+  return
+end
+
 let
     n = 10
     m = 30
@@ -11,8 +54,8 @@ let
     maxdim = 7
    
     # Make an n×m matrix of approximate rank k
+    #M = randn(n,m)
     M = randn(n,k)*randn(k,m) + eps*randn(n,m)
-    M = randn(n,m) 
     
     # added by Meng 09/27/2024
     sparsity = 0.93
