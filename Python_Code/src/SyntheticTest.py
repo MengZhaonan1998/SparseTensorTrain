768de61869f3777adf6add76f578dadbff025673
MengMeng <mengzn1998@gmail.com>
2024-10-23 23:10:16 -0400

    synthetic test Python

--- Python_Code/src/SyntheticTest.py
+++ Python_Code/src/SyntheticTest.py
@@ -12,16 +12,12 @@ from Utils import TensorSparseStat
 
 
 def testCase1():
-    '''
-    20x20x20 tensor (8000 size) -> 20x10 + 10x20x10 + 20x10 factors (2400 size)
-    Sparse ...
-    '''
     print("Unit test 1 starts!")
     
-    rank = [1, 10, 10, 1]       # TT rank
-    order = [20, 20, 20]        # tensor order
-    density = [0.1, 0.1, 0.1]   # density for every factor
-    seed = [100, 70, 4]            # random seeds
+    rank = [1, 30, 30, 1]       # TT rank
+    order = [50, 50, 50]        # tensor order
+    density = [0.01, 0.01, 0.01]   # density for every factor
+    seed = [13, 0, 3033]            # random seeds
     factors = []                # factor list 
 
     # Construct sparse tensor factors in a sparse format
@@ -32,21 +28,33 @@ def testCase1():
         factor = sp.random(shape, density = density[i], random_state=seed[i])
         factors.append(factor)                
     SpT = tl.tt_to_tensor(factors)      # Tensorly supports sparse backends
+         
+    tnsName = "syn_order_" + "_".join(map(str, order)) + "_synrank_" + "_".join(map(str, rank)) + ".tns"
+    tnsPath = "/home/mengzn/Desktop/TensorData/" + tnsName
+    cntData = SpT.nnz
+    nnzData = SpT.data
+    coord = SpT.coords
+    with open(tnsPath, "w") as f:
+        for i in range(cntData):
+            f.write(f"{coord[0][i]} {coord[1][i]} {coord[2][i]} {nnzData[i]}\n")
+    
     SpTd = SpT.todense()
     TensorSparseStat([SpTd])
     
     # TT SVD
-    factors = tensor_train(SpTd, rank)
+    rank_max = max(rank)
+    eps = 1e-10
+    factors = ttsvd(SpTd, rank_max, eps)
     reconT = tl.tt_to_tensor(factors)
     error = tl.norm(reconT - SpTd, 2) / tl.norm(SpTd, 2)
     print(f"The reconstruction error of TT-SVD is {error}")
     TensorSparseStat(factors)
     
     # TT Cross 
-    random_state = 1
+    random_state = 10
     tol = 1e-10
     maxiter = 500
-    rank = [1, 9, 10, 1]
+    rank = [1, 30, 30, 1]
     #factors = sparse_ttcross(SpTd, rank, tol, maxiter, random_state)
     factors = tensor_train_cross(SpTd, rank, tol, maxiter, random_state)
     # Check the reconstruction error and sparsity information
@@ -59,15 +67,11 @@ def testCase1():
     return
 
 def testCase2():
-    '''
-    10x20x20x10 tensor (40000 size) -> 5x10 + 5x20x10 + 5x20x10 + 5x10 factors (2100 size)
-    Sparse ...
-    '''
     print("Unit test 2 starts!")
     
-    rank = [1, 5, 10, 5, 1]         # TT rank
-    order = [10, 20, 20, 10]        # tensor order
-    density = [0.2, 0.1, 0.1, 0.2]  # density for every factor
+    rank = [1, 50, 200, 50, 1]         # TT rank
+    order = [50, 50, 50, 50]        # tensor order
+    density = [0.01, 0.01, 0.01, 0.01]  # density for every factor
     seed = [1, 2, 3, 4]             # random seeds
     factors = []                    # factor list 
 
@@ -77,6 +81,16 @@ def testCase2():
         factor = sp.random(shape, density=density[i], random_state=seed[i])
         factors.append(factor)                
     SpT = tl.tt_to_tensor(factors)            # Tensorly supports sparse backends
+    
+    tnsName = "syn_order_" + "_".join(map(str, order)) + "_synrank_" + "_".join(map(str, rank)) + ".tns"
+    tnsPath = "/home/mengzn/Desktop/TensorData/" + tnsName
+    cntData = SpT.nnz
+    nnzData = SpT.data
+    coord = SpT.coords
+    with open(tnsPath, "w") as f:
+        for i in range(cntData):
+            f.write(f"{coord[0][i]} {coord[1][i]} {coord[2][i]} {coord[3][i]} {nnzData[i]}\n")
+    
     SpTd = SpT.todense()
     TensorSparseStat([SpTd])
 
@@ -91,6 +105,7 @@ def testCase2():
     random_state = 1
     tol = 1e-10
     maxiter = 500
+    rank = [1, 20, 20,20,1]
     factors = tensor_train_cross(SpTd, rank, tol, maxiter, random_state)    
     # Check the reconstruction error and sparsity information
     reconT = tl.tt_to_tensor(factors)
@@ -145,12 +160,6 @@ def testCase3():
     print("Unit test 3 ends!\n")
     return
 
-
-
-testCase1()
-#testCase2()
+#testCase1()
+testCase2()
 #testCase3()
-
-
-# NOTE:
-# control variable: Dimension, shape...?
