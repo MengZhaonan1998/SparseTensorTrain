e5427ec131405a7c3e7a6b039a334e855b4bd2e9
MengMeng <mengzn1998@gmail.com>
2024-10-28 23:27:01 -0400

    C++ project

---
+++ C_Code/src/algo_test/synthetic_test.cpp
@@ -0,0 +1,8 @@
+#include "header.h"
+
+int main(int argc, char** argv) 
+{
+    std::cout << "Test of synthetic tensors starts!" << std::endl;
+    
+    return 0; 
+}
--- C_Code/src/ttsvd.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    int i = 1;
-    std::cout << "Tensor Train Singular Value Decomposition"; 
-    return 0;
-}
