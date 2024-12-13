26aeb64ef418c5a4109a8baea16d827eb46b1162
MengMeng <mengzn1998@gmail.com>
2024-12-13 01:35:56 -0500

    scf molecule eri tensor test 1 & 2

--- Python_Code/src/tensortrain_id.py
+++ Python_Code/src/tensortrain_id.py
@@ -1,8 +1,40 @@
 import numpy as np
 import numpy.linalg as la
 import tensorly as tl
-from interpolative_decomposition import interpolative_nuclear, interpolative_qr
-#import sparse as sp
+from interpolative_decomposition import interpolative_nuclear, interpolative_qr, interpolative_prrldu
+
+def TT_IDPRRLDU(tensorX: tl.tensor, r_max: int, eps: float, verbose: int = 0) -> list[tl.tensor]:
+    shape = tensorX.shape  # Get the shape of input tensor: [n1, n2, ..., nd]
+    dim = len(shape)       # Get the number of dimension
+    delta = (eps / np.sqrt(dim - 1)) * tl.norm(tensorX, 2)  # Truncation parameter
+    
+    W = tensorX        # Copy tensor X -> W
+    nbar = W.size      # Total size of W
+    r = 1              # Rank r
+    ttList = []        # list storing tt factors
+    iterlist = list(range(1, dim))  # Create iteration list: 1, 2, ..., d-1
+    iterlist.reverse()              # Reverse the iteration list: d-1, ..., 1 
+    verbose = 1
+    
+    for i in iterlist:
+        W = tl.reshape(W, [int(nbar / r / shape[i]), int(r * shape[i])])  # Reshape W       
+        C, X, cols, error = interpolative_prrldu(W, cutoff=delta, maxdim=r_max)
+        ri = C.shape[1]  # r_i-1 = min(r_max, r_delta_i)
+    
+        if verbose == 1:
+            rerror = tl.norm(C @ X - W, 2) / tl.norm(W, 2)
+            print(f"Iteration {i} -- low rank id approximation error = {rerror}")
+    
+        Ti = tl.reshape(X[0:ri, :], [ri, shape[i], r])
+        nbar = int(nbar * ri / shape[i] / r)  # New total size of W
+        r = ri             # Renewal r
+        W = C[:, 0:ri]     # W = U[..] * S[..]
+        ttList.append(Ti)  # Append new factor
+    
+    T1 = tl.reshape(W, [1, shape[0], r])
+    ttList.append(T1)    
+    ttList.reverse()
+    return ttList
 
 def TT_IDscatter(tensorX: tl.tensor, r_max: int, eps: float, verbose: int = 0) -> list[tl.tensor]:
     shape = tensorX.shape  # Get the shape of input tensor: [n1, n2, ..., nd]
@@ -69,16 +101,20 @@ def TT_IDQR(tensorX: tl.tensor, r_max: int, verbose: int = 0) -> list[tl.tensor]
     ttList.reverse()
     return ttList
 
-'''
-tensor = np.zeros([5,10,6,7,11])
-for i in range(5):
-    for j in range(10):
-        for m in range(6):
-            for n in range(7):
-                for l in range(11):
-                    tensor[i,j,m,n,l] = i * j - m + n * l     
-ttList = TT_IDQR(tensor, 100)
-reconTensor = tl.tt_to_tensor(ttList)
-error = tl.norm(reconTensor-tensor,2)
-pass
-'''
+def toy_test():
+    shape = [4, 5, 8, 3]
+    rank = [1, 3, 6, 2, 1]
+    ttfactor = tl.random.random_tt(shape, rank)
+    tensor = tl.tt_to_tensor(ttfactor)
+    ttfactor = TT_IDPRRLDU(tensor, 10, 1e-10)
+    reconT = tl.tt_to_tensor(ttfactor)
+    diff = np.abs(reconT - tensor)
+    error = np.max(diff)
+    for i in range(len(shape)):
+        factor = ttfactor[i]
+        print(f"Factor {i}: {factor.shape}")
+    print(f"the maximum error is {error}")
+    return
+
+#toy_test()
+
--- Python_Code/src/test_scfmolecule.py
+++ Python_Code/src/test_scfmolecule.py
@@ -1,13 +1,101 @@
-from pyscf import gto, ao2mo
 import numpy as np
-import h5py
-def view(h5file, dataname='eri_mo'):
-    with h5py.File(h5file) as f5:
-        print('dataset %s, shape %s' % (str(f5.keys()), str(f5[dataname].shape)))
-mol = gto.M(atom='O 0 0 0; H 0 1 0; H 0 0 1', basis='sto3g')
-mo1 = np.random.random((mol.nao_nr(), 10))
-mo2 = np.random.random((mol.nao_nr(), 8))
-mo3 = np.random.random((mol.nao_nr(), 6))
-mo4 = np.random.random((mol.nao_nr(), 4))
-eri1 = ao2mo.kernel(mol, mo1)
-pass
+import tensorly as tl
+import matplotlib.pyplot as plt
+from pyscf import gto, scf
+
+from utils import TensorSparseStat
+from tensortrain_id import TT_IDPRRLDU
+from tensortrain_svd import TT_SVD
+
+def view_tensor_2d(eri_tensor):
+    # View the eri tensor in 2D way
+    shape = eri_tensor.shape
+    eri2d = tl.reshape(eri_tensor, [shape[0] * shape[1], shape[2] * shape[3]])
+    plt.imshow(eri2d, interpolation='none')
+    plt.show()
+
+def disp_eritt_info(eri_tensor, eri_tt, remark = None):
+    # Display information of the eri tensor and the corresponding tensor train
+    print(f"=== INFO DISPLAY ({remark})===")
+    
+    print("--- Sparsity of the eri tensor ---")
+    TensorSparseStat([eri_tensor])
+    
+    recon_eri = tl.tt_to_tensor(eri_tt)
+    diff = np.abs(recon_eri - eri_tensor)
+    error = np.max(diff)
+    print("--- Reconstruction error ---")
+    print(f"Absolute max error: {error}")
+    print(f"Relative max error: {error / np.max(np.abs(eri_tensor))}")
+    
+    print("--- Sparsity of the eri tensor train ---")
+    TensorSparseStat(eri_tt)    
+    return
+
+def eri_test1():
+    # Electron repulsion integral (eri) tensor test 1
+    # Define the molecule
+    mol = gto.Mole()
+    mol.atom = 'Li 0 0 0; H 1.5 0 0'
+    mol.basis = 'sto-3g'  # Very minimal basis
+    mol.build()
+    
+    # Compute full ERI tensor
+    eri_tensor = mol.intor('int2e')
+    
+    # Print some information about the ERI tensor
+    print("ERI Tensor Shape:", eri_tensor.shape)
+    print("ERI Tensor Data Type:", eri_tensor.dtype)
+    # The indices represent (i,j|k,l) in physicist's notation
+    print("Example ERI element (0,0|0,0):", eri_tensor[0,0,0,0])
+    
+    # TT-ID based on PRRLDU
+    maxdim = 30
+    cutoff = 1e-6
+    eri_tt_id = TT_IDPRRLDU(eri_tensor, maxdim, cutoff)
+    disp_eritt_info(eri_tensor, eri_tt_id, "TT-IDPRRLDU")
+    
+    # TT-SVD
+    cutoff = 1e-6
+    eri_tt_svd = TT_SVD(eri_tensor, maxdim, cutoff)
+    disp_eritt_info(eri_tensor, eri_tt_svd, "TT-SVD")
+    return
+
+def eri_test2():
+    # Electron repulsion integral (eri) tensor test 2
+    # Define the molecule
+    mol = gto.Mole()
+    mol.atom = '''
+    O  0.0  0.0  0.0
+    H  0.7  0.0  0.0
+    H  0.0  0.7  0.0
+    '''
+    mol.basis = 'cc-pvdz'  # Choose a basis set
+    mol.build()
+
+    # Create a Hartree-Fock object
+    mf = scf.RHF(mol)
+
+    # Compute the ERI tensor
+    eri_tensor = mol.intor('int2e')
+
+    # Print some information about the ERI tensor
+    print("ERI Tensor Shape:", eri_tensor.shape)
+    print("ERI Tensor Data Type:", eri_tensor.dtype)
+    # The indices represent (i,j|k,l) in physicist's notation
+    print("Example ERI element (0,0|0,0):", eri_tensor[0,0,0,0])
+
+    # TT-ID based on PRRLDU
+    maxdim = 300
+    cutoff = 1e-8
+    eri_tt_id = TT_IDPRRLDU(eri_tensor, maxdim, cutoff)
+    disp_eritt_info(eri_tensor, eri_tt_id, "TT-IDPRRLDU")
+    
+    # TT-SVD
+    cutoff = 1e-7
+    eri_tt_svd = TT_SVD(eri_tensor, maxdim, cutoff)
+    disp_eritt_info(eri_tensor, eri_tt_svd, "TT-SVD")
+    return
+
+eri_test1()
+#eri_test2()
