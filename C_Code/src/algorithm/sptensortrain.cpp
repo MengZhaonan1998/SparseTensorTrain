33e6ebb0176e5a35aaa95e38d2a6e2ca773163c5
MengMeng <mengzn1998@gmail.com>
2025-01-20 01:07:09 -0500

    sparse TT

--- C_Code/src/algorithm/sptensortrain.cpp
+++ C_Code/src/algorithm/sptensortrain.cpp
@@ -6,14 +6,13 @@
 
 // Define the template function TT_ID_sparse
 template<typename T, size_t Order>
-void TT_ID_sparse(const COOTensor<T, Order>& tensor, double const cutoff, 
+SparseTTRes TT_ID_sparse(const COOTensor<T, Order>& tensor, double const cutoff, 
                 double const spthres, size_t const r_max, bool verbose) {
     // Initial settings
     auto shape = tensor.get_dimensions();  // Get the shape of the input tensor: [n1, n2, ..., nd]
     int dim = shape.size();                // Get the number of dimension d    
     size_t nbar = 1;                       // Get the total size: n1 * n2 * ... * nd 
-    for (int i = 0; i < dim; ++i)
-        nbar *= shape[i];
+    for (int i = 0; i < dim; ++i) nbar *= shape[i];
 
     // Reshape the input tensor to the 2D matrix
     size_t r = 1;  // Rank
@@ -21,8 +20,11 @@ void TT_ID_sparse(const COOTensor<T, Order>& tensor, double const cutoff,
     size_t col = r * shape[dim - 1];         // Initial matrix column
     COOMatrix_l2<T> W = tensor.reshape2Mat(row, col);
 
+    // Initialize a result list, start TT iteration
+    SparseTTRes ResList; 
+    ResList.InterG.resize(dim - 2);
     for (int i = dim - 1; i > 0; i--) {
-        std::cout << "TT ITERATION " << i << std::endl;
+        std::cout << "Tensor train iteration " << i << " starts..." <<  std::endl;
         
         // Reshape matrix (skip the first iteration)
         if (i != dim - 1) {            
@@ -37,15 +39,13 @@ void TT_ID_sparse(const COOTensor<T, Order>& tensor, double const cutoff,
         // There is no cutoff selection. Rank is revealed automatically by IDQR
         size_t ri = idResult.output_rank;
         
+        auto Z = dcoeffZRecon(idResult.interp_coeff, idResult.pivot_cols, ri, col);
+        //std::cout << "Factor Z nnz: " << Z.nnz_count << ", density: " << double(Z.nnz_count) / Z.cols / Z.rows << std::endl;
+
         // Print the low rank approximation error and sparse information
         if (verbose) {            
             auto C = W.subcol(idResult.pivot_cols, ri);
-            auto Z = dcoeffZRecon(idResult.interp_coeff, idResult.pivot_cols, ri, col);            
-            //std::cout << "C:\n"; C.print();
-            //std::cout << "Z:\n"; Z.print();
-
-            // There are bugs in .multiply function?
-            //auto recon = C.multiply(Z);
+            //auto recon = C.multiply(Z);     // There are bugs in .multiply function?
             T max_error = 0.0;            
             for (size_t i = 0; i < W.rows; ++i)
                 for (size_t j = 0; j < W.cols; ++j) {
@@ -57,25 +57,44 @@ void TT_ID_sparse(const COOTensor<T, Order>& tensor, double const cutoff,
                     max_error = std::max(max_error, abs_error);
                 }
             std::cout << "Interpolative reconstruction error: " << max_error << std::endl;   
-            std::cout << "Factor Z nnz: " << Z.nnz_count << ", density: " << double(Z.nnz_count) / Z.cols / Z.rows << std::endl;
         }
 
         // Form a new tensor-train factor
-        // TODO...
-
+        if (i == dim - 1) {
+            ResList.EndG.reset(Z.nnz_count, ri, shape[i]);
+            ResList.EndG.nnz_count = Z.nnz_count;
+            std::copy(Z.values, Z.values + Z.nnz_count, ResList.EndG.values);
+            std::copy(Z.row_indices, Z.row_indices + Z.nnz_count, ResList.EndG.indices[0]);
+            std::copy(Z.col_indices, Z.col_indices + Z.nnz_count, ResList.EndG.indices[1]);
+        } else {
+            ResList.InterG[i-1].reset(Z.nnz_count, ri, shape[i], r);
+            ResList.InterG[i-1].nnz_count = Z.nnz_count;
+            std::copy(Z.values, Z.values + Z.nnz_count, ResList.InterG[i-1].values);
+            std::copy(Z.row_indices, Z.row_indices + Z.nnz_count, ResList.InterG[i-1].indices[0]);
+            for (size_t n = 0; n < Z.nnz_count; ++n) {
+                ResList.InterG[i-1].indices[1][n] = Z.col_indices[n] / r; 
+                ResList.InterG[i-1].indices[2][n] = Z.col_indices[n] % r;
+            }
+        }
+        
         // Form new W from the interpolative factor C
         nbar = nbar * ri / shape[i] / r;  // New total size of W
         r = ri;
         W = W.subcol(idResult.pivot_cols, ri);
     }
 
-    // Memory release...
+    // Append the last factor
+    ResList.StartG.reset(W.nnz_count, W.rows, W.cols);
+    ResList.StartG.nnz_count = W.nnz_count;
+    std::copy(W.values, W.values + W.nnz_count, ResList.StartG.values);
+    std::copy(W.row_indices, W.row_indices + W.nnz_count, ResList.StartG.indices[0]);
+    std::copy(W.col_indices, W.col_indices + W.nnz_count, ResList.StartG.indices[1]);
 
-    return;
+    return ResList;
 }
 
 // Explicitly instantiate the specializations
-template void TT_ID_sparse<double, 3>(const COOTensor<double, 3>&, double, double, size_t, bool);
-template void TT_ID_sparse<double, 4>(const COOTensor<double, 4>&, double, double, size_t, bool);
-template void TT_ID_sparse<double, 5>(const COOTensor<double, 5>&, double, double, size_t, bool);
-template void TT_ID_sparse<double, 6>(const COOTensor<double, 6>&, double, double, size_t, bool);
+template SparseTTRes TT_ID_sparse<double, 3>(const COOTensor<double, 3>&, double, double, size_t, bool);
+template SparseTTRes TT_ID_sparse<double, 4>(const COOTensor<double, 4>&, double, double, size_t, bool);
+template SparseTTRes TT_ID_sparse<double, 5>(const COOTensor<double, 5>&, double, double, size_t, bool);
+template SparseTTRes TT_ID_sparse<double, 6>(const COOTensor<double, 6>&, double, double, size_t, bool);
--- C_Code/src/synthetic_test/sparse_test.cpp
+++ C_Code/src/synthetic_test/sparse_test.cpp
@@ -7,14 +7,15 @@
 void test1()
 {
     // Initialize the synthetic sparse tensor
-    COOTensor<double, 2> G1(10, 20, 10);    
-    COOTensor<double, 3> G2(10, 10, 30, 30);
-    COOTensor<double, 3> G3(10, 30, 30, 5);
-    COOTensor<double, 2> G4(10, 5, 20);
-    G1.generate_random(0.1, Distribution::UNIFORM, DistributionParams::uniform(0.0, 1.0), 100);
-    G2.generate_random(0.05, Distribution::UNIFORM, DistributionParams::uniform(0.0, 1.0), 200);
-    G3.generate_random(0.05, Distribution::UNIFORM, DistributionParams::uniform(0.0, 1.0), 300);
-    G4.generate_random(0.1, Distribution::UNIFORM, DistributionParams::uniform(0.0, 1.0), 400);
+    util::Timer timer("test1");
+    COOTensor<double, 2> G1(100, 20, 10);    
+    COOTensor<double, 3> G2(100, 10, 25, 30);
+    COOTensor<double, 3> G3(100, 30, 50, 9);
+    COOTensor<double, 2> G4(100, 9, 11);
+    G1.generate_random(0.1, Distribution::UNIFORM, DistributionParams::uniform(0.0, 10.0), 100);
+    G2.generate_random(0.03, Distribution::UNIFORM, DistributionParams::uniform(0.0, 10.0), 200);
+    G3.generate_random(0.03, Distribution::UNIFORM, DistributionParams::uniform(0.0, 10.0), 300);
+    G4.generate_random(0.05, Distribution::UNIFORM, DistributionParams::uniform(0.0, 10.0), 400);
     auto T = SparseTTtoTensor<double>(G1, G2, G3, G4);
     
     std::cout << "INPUT SETTINGS\n"; 
@@ -23,18 +24,39 @@ void test1()
     std::cout << "Input core G3 - density: " << G3.get_density() << "\n"; 
     std::cout << "Input core G4 - density: " << G4.get_density() << "\n";
     std::cout << "Synthetic tensor - nnz " << T.nnz() << ", density: " << T.get_density() << "\n";
-
-
-    size_t r_max = 100;
+    
+    size_t r_max = 30;
     double eps = 1e-8;
     double spthres = 0.3;
-    bool verbose = true;
+    bool verbose = false;
     
     if (1) {
-        TT_ID_sparse(T, eps, spthres, r_max, verbose);
+        std::cout << "TT_ID_sparse\n";
+        auto ttList = TT_ID_sparse(T, eps, spthres, r_max, verbose);
+
+        std::cout << "fac 1\n";
+        ttList.StartG.print();
+
+        std::cout << "fac 2\n";
+        ttList.InterG[0].print();
+
+        std::cout << "fac 3\n";
+        ttList.InterG[1].print();
+
+        std::cout << "fac 4\n";
+        ttList.EndG.print();
+
+        //auto reconT = SparseTTtoTensor<double>(ttList.StartG, ttList.InterG[0], ttList.InterG[1], ttList.EndG);
+        //reconT.print();
+
+        //double err = T.rel_diff(reconT);
+        //std::cout << "error: " << err << std::endl;
+        
+
     } else {
+        std::cout << "TT_IDPRRLDU_dense\n";
         auto T_full = T.to_dense();
-        TT_IDPRRLDU_dense(T_full, r_max, eps);
+        auto ttList = TT_IDPRRLDU_dense(T_full, r_max, eps);
     }
 
     return;
@@ -43,5 +65,6 @@ void test1()
 int main(int argc, char** argv) 
 { 
     test1();
+    util::Timer::summarize();
     return 0; 
 } 
